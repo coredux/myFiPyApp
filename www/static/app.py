839dd9a9c02b4c96010a362673a30b31ffcb0955
sr@SR-DELL <sr@SR-DELL>
2015-11-30 23:14:35 +0800

    upate ip addr for my virtual srv

---
+++ www/static/app.py
@@ -0,0 +1,23 @@
+__author__ = 'sr'
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>It works!</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '192.168.56.108', 8081)
+    logging.info('server started at http://192.168.56.108:8081...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
