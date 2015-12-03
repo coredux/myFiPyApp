e385401c19c7084a8a58f728a2f795dfc10e8576
coredux <redpath000@gmail.com>
2015-12-03 17:05:33 +0800

    var name

--- practice/Coroutine.py
+++ practice/Coroutine.py
@@ -3,14 +3,14 @@ __author__ = 'sr'
 # 协程练习
 # consumer为消费者
 # produce负责生产产品
-# 消费者最多进行maxTimes次消费操作
+# 消费者最多进行maxConsumeTimes次消费操作
 
 # note: this is a generator
 def consumer():
     cnt = 0
-    maxTimes = 8
+    maxConsumeTimes = 8
     print("consumer initialized")
-    while cnt < maxTimes:
+    while cnt < maxConsumeTimes:
         numProduct = yield # waiting for producing
 
         if not numProduct:
@@ -18,14 +18,15 @@ def consumer():
         else:
             print('consumer: now the number of products is %d' % numProduct )
         cnt += 1
-        
+
     return
 
 def produce( consGen ):
     consGen.send( None )
     numProduct = 0
+    maxProduceTimes = 15
     print("producer initialized")
-    while numProduct < 15:
+    while numProduct < maxProduceTimes:
         try:
             consGen.send( numProduct )
         except:
